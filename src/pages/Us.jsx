49ac63e8ef62410fc3390f4091ce6ed880103d3c
Engels Baca Sevilla <engels@engveloper.com>
2022-06-10 23:42:42 -0600

    update 'us' page

--- src/pages/Us.jsx
+++ src/pages/Us.jsx
@@ -1,3 +1,22 @@
 export function Us() {
-  return <h1>Us</h1>;
+  return (
+    <div>
+      <h1>Nosotros</h1>
+      <p className="text-center">
+        Somos un grupo de especialistas en distintos sectores del Desarrollo Web
+        y Marketing Online, tras trabajar durante años para empresas, decidimos
+        crear nuestra propia agencia y poder brindar al cliente final un
+        producto asequible y de la más alta calidad.
+      </p>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/aQlkVB9LZPk"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
